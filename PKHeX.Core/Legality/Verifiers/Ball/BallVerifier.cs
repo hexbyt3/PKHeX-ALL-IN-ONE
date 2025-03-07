5f26b83df554c3e2f41422dc534646f564dfbcd1
Gengar <80122551+bdawg1989@users.noreply.github.com>
2025-03-06 19:39:27 -0600

    Add latest raid distribution data 🔥🐊🎤

--- PKHeX.Core/Legality/Verifiers/Ball/BallVerifier.cs
+++ PKHeX.Core/Legality/Verifiers/Ball/BallVerifier.cs
@@ -140,9 +140,10 @@ public sealed class BallVerifier : Verifier
             return GetInvalid(LBallUnavailable);
 
         // Sprigatito: Meowscarada the Unrivaled
-        // Fuecoco, Quaxly: Pokémon GO
+        // Fuecoco: Skeledirge the Unrivaled
+        // Quaxly: Pokémon GO
         var species = enc.Species;
-        if (species is >= (int)Species.Fuecoco and <= (int)Species.Quaquaval)
+        if (species is >= (int)Species.Quaxly and <= (int)Species.Quaquaval)
             return VerifyBallEquals(ball, BallUseLegality.WildPokeballs8g_WithoutRaid);
 
         var result = BallContextHOME.Instance.CanBreedWithBall(enc.Species, enc.Form, ball, pk);
